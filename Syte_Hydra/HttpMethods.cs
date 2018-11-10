7ba518c90de0ce280897a3b303c69fda35c39357
Gleb <glebon1802@gmail.com>
2018-11-10 22:31:14 +0200

    Unexpected Bad Response

--- Syte_Hydra/HttpMethods.cs
+++ Syte_Hydra/HttpMethods.cs
@@ -14,6 +14,8 @@ namespace Syte_Hydra
             HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
             req.Method = "GET";
             req.CookieContainer = cookies;
+            req.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
+            req.ContentType = "application/x-www-form-urlencoded";
             req.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36";
             req.Referer = referer;
 
@@ -36,35 +38,31 @@ namespace Syte_Hydra
             HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
             req.Method = "POST";
             req.CookieContainer = cookies;
-            req.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36";
+            req.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36";
             req.Referer = referer;
             req.ContentType = "application/x-www-form-urlencoded";
             req.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
 
             using (Stream postStream = req.GetRequestStream())
             {
-                byte[] postBytes = Encoding.ASCII.GetBytes(postData);
+                byte[] postBytes = Encoding.Unicode.GetBytes(postData);
                 postStream.Write(postBytes, 0, postBytes.Length);
             }
 
+            string pageSrc = "";
             try
             {
                 HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
                 cookies.Add(resp.Cookies);
-                Console.WriteLine(resp.Headers);
+                using (StreamReader sr = new StreamReader(resp.GetResponseStream()))
+                {
+                    pageSrc = sr.ReadToEnd();
+                }
             }
-            catch(WebException ex)
+            catch (WebException ex)
             {
                 Console.WriteLine(ex.Response.Headers);
             }
-
-
-
-            string pageSrc = "";
-            //using (StreamReader sr = new StreamReader(resp.GetResponseStream()))
-            //{
-            //    pageSrc = sr.ReadToEnd();
-            //}
 
             return (!pageSrc.Contains(key));
         }
--- Syte_Hydra/Program.cs
+++ Syte_Hydra/Program.cs
@@ -20,7 +20,7 @@ namespace Syte_Hydra
 
             string postData = String.Format("ReturnUrl=%2F&AuthEmail={0}&AuthPassword={1}&__SART={2}%3D", username, password, token);
 
-            bool result = HttpMethods.Post(url + "auth/login/", postData, "https://www.twirpx.com/", myCookies);
+            bool result = HttpMethods.Post(url + "auth/login/", postData, referer, myCookies);
 
 
             if (result)
@@ -35,10 +35,31 @@ namespace Syte_Hydra
         //<input type="hidden" name="user_token" value="45f1f9e364bda3b155ef96e890f1474f">
         static string GetBetween(string msg, string start, string stop)
         {
-            int startIndex = msg.IndexOf(start) + start.Length;
-            int stopIndex = msg.IndexOf(stop, startIndex);
+            var tokens = new List<String>();
+            int i1 = 0, i2 = 0;
 
-            return msg.Substring(startIndex, stopIndex - startIndex);
+            while(true)
+            {
+                int tmp= msg.IndexOf(start, i2);
+                if (tmp > 0)
+                {
+                    i1 = tmp + start.Length;
+                    i2 = msg.IndexOf(stop, i1);
+                    tokens.Add(msg.Substring(i1, i2 - i1));
+                }
+                else
+                    break;
+            }
+            foreach (var elem in tokens)
+                Console.WriteLine(elem);
+
+            tokens[1] = tokens[1].Replace("&#x2B;", "+");
+            return tokens[1];
+
+            //int startIndex = msg.IndexOf(start) + start.Length;
+            //int stopIndex = msg.IndexOf(stop, startIndex);
+            //Console.WriteLine(msg.Substring(startIndex, stopIndex - startIndex));
+            //return msg.Substring(startIndex, stopIndex - startIndex);
         }
     }
 }
